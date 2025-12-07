025675ccd57ec15529331321c3c1027e290c5d59
amirphpp800 <hpopenai2024@gmail.com>
2025-12-07 17:09:01 +0330

    Update dashboard.js

--- public/js/components/dashboard.js
+++ public/js/components/dashboard.js
@@ -36,7 +36,6 @@ const Dashboard = {
             ${Header.render('داشبورد', false, true)}
             <div class="page" style="padding-bottom: 100px;">
                 <div class="container">
-                    ${this.renderProStatus()}
                     ${this.renderUsageStats()}
                     ${await this.renderQuickActions()}
                     ${isAdmin ? this.renderAdminAccess() : ''}
